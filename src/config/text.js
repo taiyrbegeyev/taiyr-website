d37fa0c80ebc2d4f02545639c6c6c020e615c82a
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-08-17 18:45:08 +0200

    :wrench: add text config

---
+++ src/config/text.js
@@ -0,0 +1,4 @@
+export default {
+  about_me: 'Hi there! I am a second year Computer Science student at Jacobs University Bremen, Germany. Besides being a student, I am a full-stack developer. I make websites for people'  
+}
+
