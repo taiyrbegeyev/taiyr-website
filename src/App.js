cda90b70c5ffbef299856a039abe5ba5dff43b79
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-08-17 16:38:02 +0200

    :lipstick: css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Intro from 'containers/Intro'
 
 class App extends Component {
   render() {
     return(
       <React.Fragment>
-        Hey
+        <Intro />
       </React.Fragment>
     )
   }
 };
 
-export default App;
+export default App
