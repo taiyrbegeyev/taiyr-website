067e3c19dfcbf057da7eb8f4e2dcebac88e0f63b
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-08-23 18:30:23 +0200

    :wrench: add text configs for projects

--- src/config/text.js
+++ src/config/text.js
@@ -1,4 +1,30 @@
-export default {
-  about_me: 'Hi there! I am a second year Computer Science student at Jacobs University Bremen, Germany. Besides being a student, I am a full-stack developer. I make websites for people'  
+const about_me = {
+  about_me: 'Hi there! I am a second year Computer Science student at Jacobs University Bremen, Germany. Besides being a student, I am a full-stack developer. I make websites for people.'
 }
 
+const projects = {
+  trump_bot: {
+    name: "Trump Bot",
+    description: "Telegram Bot that can search for the dumbest things Donald Trump has ever said by keyword. If you want some diversity, random quote and meme options are available.",
+    link: "https://www.github.com/taiyrbegeyev/trump_genius_bot"
+  },
+  weather_app: {
+    name: "Weather Application",
+    description: "Weather App that fetches API from openweathermap. User is asked to input a city and a country he or she wants to get information about. The temperature, humidity and conditions are displayed to the user.",
+    link: "https://www.github.com/taiyrbegeyev/weather-app"
+  },
+  campus_life: {
+    name: "Campus Life",
+    description: "Work on the front-end and back-end of the unified Campus Website : Campus Store, Campus Life and Campus People.",
+    link: "https://www.github.com/techclubjacobs/"
+  },
+  browser_control: {
+    name: "Browser Control",
+    description: "Arduino project that deals with controlling a web browser using hand gestures. It uses ultrasonic sensors to detect the motion of the hand and utilise Arduino and Python to manage a web browser. It allows hand gestures to carry out certain functions on a web browser such as scrolling up and down or switching to different tabs open in a window.",
+    link: "https://www.github.com/taiyrbegeyev/Browser-Control"
+  }
+}
+
+export {
+  about_me, projects
+}
